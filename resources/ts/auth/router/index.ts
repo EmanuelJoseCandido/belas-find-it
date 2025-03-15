b185fe9a928011835979b3ad9cd39e41333a3fc6
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2025-03-15 06:32:31 +0100

    :recycle: Refatora rotas de autenticação, adiciona suporte a registro e login com redirecionamento

--- resources/ts/auth/router/index.ts
+++ resources/ts/auth/router/index.ts
@@ -1,4 +1,21 @@
 import type { RouteRecordRaw } from "vue-router";
-import Login from "../pages/Login.vue";
 
-export const authRoutes: Array<RouteRecordRaw> = [{ path: "/auth", component: Login }];
+export const authRoutes: Array<RouteRecordRaw> = [
+    {
+        path: "/auth",
+        name: 'auth',
+        redirect: { name: "auth-login" },
+        children: [
+            {
+                path: "register",
+                name: "auth-register",
+                component: () => import("../pages/Register.vue"),
+            },
+            {
+                path: "login",
+                name: "auth-login",
+                component: () => import("../pages/Login.vue"),
+            },
+        ],
+    },
+];
