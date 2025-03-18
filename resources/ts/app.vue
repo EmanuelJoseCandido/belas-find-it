7d407318125aa4796949d8672343b8b136edd5ad
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2025-03-18 11:30:52 +0100

    :sparkles: Adiciona componente Toaster ao app.vue para exibição de notificações.

--- resources/ts/app.vue
+++ resources/ts/app.vue
@@ -1,7 +1,11 @@
 <template>
+    <Toaster />
     <router-view v-slot="{ Component, route }">
         <div :key="route.name">
             <Component :is="Component" />
         </div>
     </router-view>
 </template>
+<script setup lang="ts">
+import { Toaster } from "@/ui/components/toast";
+</script>
