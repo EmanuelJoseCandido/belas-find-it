17207227dfbc2593d9f81aca41faddcf6290f62d
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2025-05-17 13:48:51 +0100

    :recycle: refatorar CheckboxField.vue para usar v-model e simplificar a lógica de emissão de eventos.

--- resources/ts/auth/components/CheckboxField.vue
+++ resources/ts/auth/components/CheckboxField.vue
@@ -1,32 +1,33 @@
 <template>
-    <div>
-        <div class="flex items-center space-x-2">
-            <Checkbox
-                :id="id"
-                :name="name"
-                :checked="modelValue"
-                @update:checked="$emit('update:modelValue', $event)"
-            />
-            <label
-                :for="id"
-                class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-                <slot></slot>
-            </label>
-        </div>
-        <p v-if="error" class="mt-1 text-sm text-red-600">{{ error }}</p>
+  <div>
+    <div class="flex items-center space-x-2">
+      <Checkbox :id="id" :name="name" v-model="isChecked" />
+      <label
+        :for="id"
+        class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        <slot></slot>
+      </label>
     </div>
+    <p v-if="error" class="mt-1 text-sm text-red-600">{{ error }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Checkbox } from "@/ui/components/checkbox";
+import { computed } from "vue";
 
-defineProps<{
-    id: string;
-    name: string;
-    modelValue: boolean;
-    error?: string;
+const props = defineProps<{
+  id: string;
+  name: string;
+  modelValue: boolean;
+  error?: string;
 }>();
 
-defineEmits(["update:modelValue"]);
+const emit = defineEmits(["update:modelValue"]);
+
+const isChecked = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
 </script>
