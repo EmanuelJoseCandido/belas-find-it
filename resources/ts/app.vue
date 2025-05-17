b10b2a3813a536bdf1ca163fdbb81efc775e45e8
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2025-05-17 12:43:02 +0100

    :recycle: remove unused imports and onMounted logic from app.vue

--- resources/ts/app.vue
+++ resources/ts/app.vue
@@ -7,13 +7,5 @@
   </router-view>
 </template>
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { useAuthStore } from "@/auth/stores/authStore";
 import { Toaster } from "@/ui/components/toast";
-
-const authStore = useAuthStore();
-
-onMounted(async () => {
-  await authStore.getMe();
-});
 </script>
