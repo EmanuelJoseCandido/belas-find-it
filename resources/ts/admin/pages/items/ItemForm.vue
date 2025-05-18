7ead518de4e0d90ff1649f64496a9b1be90cf129
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2025-05-18 18:35:10 +0100

    :recycle: adicionar componentes de página para Dashboard, Settings, CategoriesList, ClaimsList, ItemForm, ItemsList e UsersList.

--- resources/ts/admin/pages/items/ItemForm.vue
+++ resources/ts/admin/pages/items/ItemForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>Dashboard</h2>
+    <h2>ItemsList</h2>
     <router-link to="/"> Take me to Test page </router-link>
   </div>
 </template>
--- resources/ts/admin/router/index.ts
+++ resources/ts/admin/router/index.ts
@@ -12,43 +12,43 @@ export const adminRoutes: Array<RouteRecordRaw> = [
                 path: "",
                 name: "admin-dashboard",
                 component: () => import("../pages/Dashboard.vue"),
-            },/*
+            },
             {
                 path: "items",
                 name: "admin-items",
-                component: () => import("../pages/Items/ItemsList.vue"),
+                component: () => import("../pages/items/ItemsList.vue"),
             },
             {
                 path: "items/create",
                 name: "admin-items-create",
-                component: () => import("../pages/Items/ItemForm.vue"),
+                component: () => import("../pages/items/ItemForm.vue"),
             },
             {
                 path: "items/:id/edit",
                 name: "admin-items-edit",
-                component: () => import("../pages/Items/ItemForm.vue"),
+                component: () => import("../pages/items/ItemForm.vue"),
             },
             {
                 path: "categories",
                 name: "admin-categories",
                 component: () =>
-                    import("../pages/Categories/CategoriesList.vue"),
+                    import("../pages/categories/CategoriesList.vue"),
             },
             {
                 path: "users",
                 name: "admin-users",
-                component: () => import("../pages/Users/UsersList.vue"),
+                component: () => import("../pages/users/UsersList.vue"),
             },
             {
                 path: "claims",
                 name: "admin-claims",
-                component: () => import("../pages/Claims/ClaimsList.vue"),
+                component: () => import("../pages/claims/ClaimsList.vue"),
             },
             {
                 path: "settings",
                 name: "admin-settings",
                 component: () => import("../pages/Settings.vue"),
-            }, */
+            },
         ],
     },
 ];
