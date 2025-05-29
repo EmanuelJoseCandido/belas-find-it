dd4e28ac48cee0a97d2ceb18b97b2b6fb32df933
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2025-05-29 06:08:24 +0100

    :recycle: ajustar a formatação do layout no WebsiteLayout.vue para melhor legibilidade e organização do código.

--- resources/ts/website/layouts/WebsiteLayout.vue
+++ resources/ts/website/layouts/WebsiteLayout.vue
@@ -1,21 +1,29 @@
 <template>
-    <div
-        class="min-h-screen flex flex-col bg-gradient-to-b from-slate-50 to-slate-100"
-    >
-        <!-- Header -->
-        <TheHeader />
+  <div
+    class="min-h-screen flex flex-col bg-gradient-to-b from-slate-50 to-slate-100"
+  >
+    <!-- Header -->
+    <TheHeader />
 
-        <!-- Main Content -->
-        <main class="flex-grow">
-            <slot />
-        </main>
+    <!-- Main Content -->
+    <main class="flex-grow">
+      <slot />
+    </main>
 
-        <!-- Footer -->
-        <TheFooter />
-    </div>
+    <!-- Footer -->
+    <TheFooter />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
 import TheHeader from "@/website/components/partials/TheHeader.vue";
 import TheFooter from "@/website/components/partials/TheFooter.vue";
+import { useAuthStore } from "@/auth/stores/authStore";
+
+const authStore = useAuthStore();
+
+onMounted(async () => {
+  await authStore.getMe();
+});
 </script>
